d8b71d8602d446efcb26c2001e5767c8dea16af6
Ishimwe Fabrice <ishimwefabrice520@gmail.com>
2024-09-19 01:30:29 +0530

    read data from user

---
+++ Access.py
@@ -0,0 +1,9 @@
+name = input("Your name please: ")
+
+age = input("Enter your age: ")
+
+fc = input("Enter your favourite color: ")
+
+
+
+print(name + " " + "has" + " " + age + "years old" + " " + "and favourite color is" + " " + fc)
